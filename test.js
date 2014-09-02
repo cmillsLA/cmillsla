a3510ca3f4ef6df8a20abf452156c126b86a888f
Chris <chrismills.me@gmail.com>
2014-09-02 13:09:17 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
 alert('external file loaded!');
 
-document.write('<script src="http://chrismills.la/test2.js type="text/javascript"></script>');
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'http://chrismills.la/test2.js';
+document.body.appendChild(script);
+
+document.write('<script src="http://chrismills.la/test2.js" type="text/javascript"></script>');
