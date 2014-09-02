f63355cbb53ca7f01d1c6db0bec40855ed637049
Chris <chrismills.me@gmail.com>
2014-09-02 13:05:14 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,3 @@
 console.log('external file loaded!');
 
-var loadExternalScript = function() {
-  document.write('<script src="http://chrismills.la/test2.js type="text/javascript"></script>');
-}
-loadExternalScript();
+document.write('<script src="http://chrismills.la/test2.js type="text/javascript"></script>');
