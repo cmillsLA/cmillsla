ac5f6edbaec7f5b166e882cc968a1b75278c07c0
Chris <chrismills.me@gmail.com>
2014-09-02 13:15:54 -0700

    Update test.js

--- test.js
+++ test.js
@@ -4,5 +4,3 @@ var script = document.createElement('script');
 script.type = 'text/javascript';
 script.src = 'http://chrismills.la/test2.js';
 document.body.appendChild(script);
-
-document.write('<script src="http://chrismills.la/test2.js" type="text/javascript"></script>');
